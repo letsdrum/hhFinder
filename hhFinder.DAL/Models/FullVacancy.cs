24117b28a70da00a6d930d4a9461d55367d0f0d5
Sergey V. <letsdrumm@gmail.com>
2020-01-15 09:38:17 +0300

    Перенес модели из слоя Entity в DAL. Слой Entity удален.

---
+++ hhFinder.DAL/Models/FullVacancy.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace hhFinder.Entities
+{
+    public class FullVacancy : VacancyModel
+    {
+        public Experience Experience { get; set; }
+
+        public Schedule Schedule { get; set; }
+
+        public Employment Employment { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        public List<Skill> KeySkills { get; set; }        
+    }
+   
+    public class Experience
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class Schedule
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+    
+    public class Employment
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class Skill
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
